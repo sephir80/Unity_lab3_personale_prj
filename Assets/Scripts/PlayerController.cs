85357a816e4a9c720a512aba6bfe71dc2bdf3d63
Alessandro Tognon <sephir80@gmail.com>
2022-05-07 16:29:59 +0200

    [feat] add force to move and limit movement in z

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed=5.0f;
+    public float zBound = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame,
+    void Update()
+    {
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput= Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+
+    }
+}
